10a5ce71bd64b868535acbce0e41c5154794dc11
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-04-12 22:35:19 +0300

    Update SQLQuery1.sql
    asd

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO Departments (Financing, Name)
+VALUES (15000, 'Computer Science');
+
+
+INSERT INTO Faculties (Dean, Name)
+VALUES ('John Doe', 'Engineering');
+
+
+INSERT INTO Groups (Name, Rating, Year)
+VALUES ('A1', 4, 1);
+
+
+INSERT INTO Teachers (EmploymentDate, IsAssistant, IsProfessor, Name, Position, Premium, Salary, Surname)
+VALUES ('2020-05-15', 0, 1, 'Jane', 'Professor', 500, 2500, 'Doe');
