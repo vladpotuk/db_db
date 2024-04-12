27fbe2fdbabd30397f1c850327d48d7e75499f8a
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-04-12 22:37:58 +0300

    Update SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,19 @@
+
+UPDATE Departments
+SET Name = 'Computer Engineering'
+WHERE Id = 1;
+
+
+UPDATE Faculties
+SET Dean = 'Jane Smith'
+WHERE Id = 2;
+
+
+UPDATE Groups
+SET Rating = 5
+WHERE Id = 3;
+
+
+UPDATE Teachers
+SET Premium = Premium + 100
+WHERE Id = 2;
