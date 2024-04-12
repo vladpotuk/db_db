eae3e0f773583baeff010d889bc5279a01690f0b
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-04-12 22:39:32 +0300

    Update SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+
+DELETE FROM Departments
+WHERE Id = 1;
+
+
+DELETE FROM Faculties
+WHERE Name = 'Faculty of Arts';
+
+
+DELETE FROM Groups
+WHERE Rating = 0;
+
+
+DELETE FROM Teachers
+WHERE Name = 'John Doe';
